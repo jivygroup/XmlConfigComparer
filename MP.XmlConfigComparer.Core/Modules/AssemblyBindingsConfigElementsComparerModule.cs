0409e08f8b709f80cf46ccd17e598b51da87d699
Offir Shvartz <offir@marketspulse.com>
2018-08-28 17:46:22 +0300

    checkpoint

--- MP.XmlConfigComparer.Core/Modules/AssemblyBindingsConfigElementsComparerModule.cs
+++ MP.XmlConfigComparer.Core/Modules/AssemblyBindingsConfigElementsComparerModule.cs
@@ -14,10 +14,32 @@ namespace MP.XmlConfigComparer.Core.Modules
 
     public Task<List<ConfigurationDiff>> Compare(XElement configElements1, XElement configElements2)
     {
-
+      RuntimeAssemblyBindings asseBindings1 = ReadAssemblyBindings(configElements1);
+      RuntimeAssemblyBindings asseBindings2 = ReadAssemblyBindings(configElements2);
    
 
       return Task.FromResult(new List<ConfigurationDiff>());
     }
+
+    private RuntimeAssemblyBindings ReadAssemblyBindings(XElement configElements2)
+    {
+      return null;
+    }
+  }
+
+  internal class RuntimeAssemblyBindings
+  {
+    public bool GcServerEnable { get; set; }
+    public List<AssemblyBinding> AssemblyBinding { get; set; }
+  }
+
+  public class AssemblyBinding
+  {
+    public string AssemblyIdentityName { get; set; }
+    public string PublicToken { get; set; }
+    public string Calture { get; set; }
+    public string OldVersion { get; set; }
+    public string NewVersion { get; set; }
+
   }
 }
