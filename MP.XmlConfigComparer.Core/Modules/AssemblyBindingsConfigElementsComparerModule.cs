b5d4e6bf7a3d314b4bc9ce553340db4389e05118
Offir Shvartz <offir@marketspulse.com>
2018-08-29 11:47:36 +0300

    first commit

--- MP.XmlConfigComparer.Core/Modules/AssemblyBindingsConfigElementsComparerModule.cs
+++ MP.XmlConfigComparer.Core/Modules/AssemblyBindingsConfigElementsComparerModule.cs
@@ -16,30 +16,145 @@ namespace MP.XmlConfigComparer.Core.Modules
     {
       RuntimeAssemblyBindings asseBindings1 = ReadAssemblyBindings(configElements1);
       RuntimeAssemblyBindings asseBindings2 = ReadAssemblyBindings(configElements2);
-   
 
-      return Task.FromResult(new List<ConfigurationDiff>());
+
+      List<ConfigurationDiff> diffList = new List<ConfigurationDiff>();
+
+      if (asseBindings1.GcServerEnable != asseBindings2.GcServerEnable)
+      {
+
+        diffList.Add(new ConfigurationDiff
+        {
+          Identifier = "GcServerEnabled",
+          ConfigurationItem1 = new ConfigurationElement
+          {
+            Value = asseBindings1.GcServerEnable.ToString() 
+          },
+          ConfigurationItem2 = new ConfigurationElement
+          {
+            Value = asseBindings2.GcServerEnable.ToString() 
+          }
+        });
+
+      }
+
+      Dictionary<string, DependentAssemblyBinding> dependentAssemblyBindingDic1 =
+        asseBindings1.DependentAssemblyBindings.ToDictionary(binding => binding.Key);
+
+      Dictionary<string, DependentAssemblyBinding> dependentAssemblyBindingDic2 =
+        asseBindings2.DependentAssemblyBindings.ToDictionary(binding => binding.Key);
+
+      var allkeys = dependentAssemblyBindingDic1.Keys.Union(dependentAssemblyBindingDic2.Keys);
+
+
+      foreach (var key in allkeys)
+      {
+        if (!dependentAssemblyBindingDic1.TryGetValue(key, out DependentAssemblyBinding configSectionInfo1) || configSectionInfo1 == null)
+        {
+          var localConfigSection = dependentAssemblyBindingDic2[key];
+          diffList.Add(new ConfigurationDiff
+          {
+            Identifier = key,
+            ConfigurationItem1 = null,
+            ConfigurationItem2 = new ConfigurationElement {Value = localConfigSection.Element?.ToString(),LineNum = localConfigSection.Element?.GetLineNumber()}
+          });
+        }
+        else if (!dependentAssemblyBindingDic2.TryGetValue(key, out DependentAssemblyBinding configSectionInfo2) || configSectionInfo2 == null)
+        {
+          var localConfigSection = dependentAssemblyBindingDic1[key];
+          diffList.Add(new ConfigurationDiff
+          {
+            Identifier = key,
+            ConfigurationItem2 = null,
+            ConfigurationItem1 = new ConfigurationElement {Value = localConfigSection.Element?.ToString(),LineNum = localConfigSection.Element?.GetLineNumber()}
+          });
+          
+        }
+
+      
+
+        else if (!IsEqual(configSectionInfo1,configSectionInfo2))
+        {
+          diffList.Add(new ConfigurationDiff
+          {
+            Identifier = key,
+            ConfigurationItem1 = new ConfigurationElement {Value = configSectionInfo1.Element?.ToString(),LineNum = configSectionInfo1.Element?.GetLineNumber()},
+            ConfigurationItem2 = new ConfigurationElement {Value = configSectionInfo2.Element?.ToString(),LineNum = configSectionInfo2.Element?.GetLineNumber()}
+          });
+        }
+
+      }
+
+      return Task.FromResult(diffList);
     }
 
-    private RuntimeAssemblyBindings ReadAssemblyBindings(XElement configElements2)
+    private bool IsEqual(DependentAssemblyBinding configSectionInfo1, DependentAssemblyBinding configSectionInfo2)
     {
-      return null;
+      if (configSectionInfo1 == null && configSectionInfo2 == null)
+      {
+        return true;
+      }
+
+      return XNode.DeepEquals(configSectionInfo1.Element, configSectionInfo2.Element);
+    }
+
+    private RuntimeAssemblyBindings ReadAssemblyBindings(XElement configElements)
+    {
+      var gcServerElement = configElements?.Element("runtime")?.Descendants("gcServer").FirstOrDefault();
+
+
+      var dependentAssemblies = configElements?.Element("runtime")
+        ?.Descendants("{urn:schemas-microsoft-com:asm.v1}assemblyBinding").FirstOrDefault()
+        ?.Descendants("{urn:schemas-microsoft-com:asm.v1}dependentAssembly").ToList();
+
+      return new RuntimeAssemblyBindings
+      {
+        GcServerEnable = GetGcServer(gcServerElement),
+        DependentAssemblyBindings = dependentAssemblies?.Select(CreateDependentAssemblyBinding ).ToList() ?? new List<DependentAssemblyBinding>()
+        
+
+      };
+    }
+
+    private DependentAssemblyBinding CreateDependentAssemblyBinding(XElement element)
+    {
+      return new DependentAssemblyBinding
+      {
+        AssemblyIdentityName = element.Descendants("{urn:schemas-microsoft-com:asm.v1}assemblyIdentity").Single().Attribute("name")?.Value,
+        Calture = element.Descendants("{urn:schemas-microsoft-com:asm.v1}assemblyIdentity").Single().Attribute("culture")?.Value,
+        PublicToken = element.Descendants("{urn:schemas-microsoft-com:asm.v1}assemblyIdentity").Single().Attribute("publicKeyToken")?.Value,
+        Element = element
+        
+      };
+    }
+
+    private bool GetGcServer(XElement gcServerElement)
+    {
+      if (gcServerElement?.Attribute("enabled") == null)
+      {
+        return false;
+      }
+
+      return bool.Parse(gcServerElement?.Attribute("enabled").Value);
     }
   }
 
   internal class RuntimeAssemblyBindings
   {
     public bool GcServerEnable { get; set; }
-    public List<AssemblyBinding> AssemblyBinding { get; set; }
+    public List<DependentAssemblyBinding> DependentAssemblyBindings { get; set; }
   }
 
-  public class AssemblyBinding
+  public class DependentAssemblyBinding
   {
     public string AssemblyIdentityName { get; set; }
     public string PublicToken { get; set; }
     public string Calture { get; set; }
-    public string OldVersion { get; set; }
-    public string NewVersion { get; set; }
+    public XElement Element { get; set; }
+
+
+    public string Key => $"{AssemblyIdentityName}_{PublicToken}_{Calture}";
+
 
   }
 }
--- MP.XmlConfigComparer.Core/Modules/ServiceModelConfigElementsComparerModule.cs
+++ MP.XmlConfigComparer.Core/Modules/ServiceModelConfigElementsComparerModule.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using System.Collections.Generic;
 using System.Threading.Tasks;
 using System.Xml.Linq;
 using MP.XmlConfigComparer.Core.Helpers;
@@ -14,10 +11,29 @@ namespace MP.XmlConfigComparer.Core.Modules
 
     public Task<List<ConfigurationDiff>> Compare(XElement configElements1, XElement configElements2)
     {
+      var serviceModelElement1 = configElements1.Element("system.serviceModel");
+      var serviceModelElement2= configElements2.Element("system.serviceModel");
 
-   
+      if (serviceModelElement1 == null && serviceModelElement2 == null)
+      {
+        return Task.FromResult(new List<ConfigurationDiff>());
+      }
 
+      if (serviceModelElement1 == null || serviceModelElement2 == null ||
+          !XNode.DeepEquals(serviceModelElement1, serviceModelElement2))
+      {
+        return Task.FromResult(new List<ConfigurationDiff>
+        {
+          new ConfigurationDiff()
+          {
+            Identifier = "system.serviceModel",
+            ConfigurationItem1 = new ConfigurationElement {Value = serviceModelElement1?.ToString(),LineNum = serviceModelElement1?.GetLineNumber() },
+            ConfigurationItem2 = new ConfigurationElement {Value = serviceModelElement2?.ToString(),LineNum = serviceModelElement2?.GetLineNumber() }
+          }
+        });
+      }
       return Task.FromResult(new List<ConfigurationDiff>());
+      
     }
   }
 }
